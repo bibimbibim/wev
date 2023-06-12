bc354fd08a3cfd641e1d0dc73bf4f75b630f54df
bibimbibim <bibimbibim20@gmail.com>
2023-06-12 15:29:14 +0000

    three

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const chromium = require('chrome-aws-lambda')
 
 app.use(express.json());
